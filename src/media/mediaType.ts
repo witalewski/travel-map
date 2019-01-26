0ee5f0d0ee2766729018b8f8d7a0453ae0e938a4
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2019-01-26 15:57:27 +0100

    add m4v to video types

--- src/media/mediaType.ts
+++ src/media/mediaType.ts
@@ -2,4 +2,4 @@ export const isImage: (filename: string) => boolean = filename =>
   Boolean(filename.match(/\.(jpe?g|png|gif|bmp|webp)$/i));
 
   export const isVideo: (filename: string) => boolean = filename =>
-  Boolean(filename.match(/\.(mp4|mov|avi|3gp|webm)$/i));
+  Boolean(filename.match(/\.(mp4|mov|avi|3gp|webm|m4v)$/i));
